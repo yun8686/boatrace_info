2930efde9a6d14aa0347a49038a43373b74984ca
yun <>
2019-08-07 21:40:56 +0900

    画面サンプルを作成

---
+++ pages/racepage/race/_raceid.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card>
+    <v-tabs
+      light
+      :show-arrows="!isMobileAny"
+    >
+      <v-tab
+        v-for="tab in tabs"
+        :key="tab.id"
+        :href="'#tab-'+tab.id"
+      >
+        {{ tab.id }}. {{tab.name }}
+      </v-tab>
+
+      <v-tab-item
+        v-for="tab in tabs"
+        :key="tab.id"
+        :value="'tab-' + tab.id"
+      >
+        <RaceCard :title="tab.name" />
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+</template>
+<script>
+  import placeInfo from '@/plugins/placeInfo.js';
+  import isMobile from 'ismobilejs';
+  import RaceCard from '../raceCard.vue';
+  export default {
+    data () {
+      console.log(this.$route.params.raceid);
+      return {
+        isMobileAny: isMobile.any,
+        tabs: placeInfo.list,
+      }
+    },
+    methods: {
+      isValid: ()=>{return false},
+    },
+    components: {
+      RaceCard,
+    }
+  }
+</script>
---
+++ pages/racepage/raceCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card flat>
+    <v-flex class="text-center">
+      <v-btn>{{title}}</v-btn>
+      <v-card-text>text</v-card-text>
+      <v-simple-table>
+        <thead>
+          <tr>
+            <th class="text-left">枠</th>
+            <th class="text-left">Name</th>
+            <th class="text-left">Calories</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in desserts" :key="item.name">
+            <td class="text-left">{{ item.id }}</td>
+            <td class="text-left">{{ item.name }}</td>
+            <td class="text-left">{{ item.calories }}</td>
+          </tr>
+        </tbody>
+      </v-simple-table>
+    </v-flex>
+  </v-card>
+</template>
+<script>
+export default {
+  props: ['title'],
+  mounted: () => {
+    console.log("マウントエッド")
+  },
+  data: ()=>{
+    return {
+      desserts: [
+        {id: 1, name: "おけ", calories: "100"},
+        {id: 2, name: "おけ", calories: "100"},
+        {id: 3, name: "おけ", calories: "100"},
+        {id: 4, name: "おけ", calories: "100"},
+        {id: 5, name: "おけ", calories: "100"},
+        {id: 6, name: "おけ", calories: "100"},
+      ]
+    }
+  }
+}
+</script>
